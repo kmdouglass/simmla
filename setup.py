80d4c2cf4026457f82abd796368dcde351a40f52
kmdouglass <kyle.m.douglass@gmail.com>
2016-01-28 10:10:42 +0100

    Fixed bug and updated URL's in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description'      : 'Fourier optics-based simulations for microlens arrays (MLAs),
+    'description'      : 'Fourier optics-based simulations for microlens arrays (MLAs)',
     'long_description' : 'SimMLA provides routines for simulating microlens arrays and flys eye condensers.',
     'author'           : 'Kyle M. Douglass',
-    'url'              : 'https://github.com/kmdouglass/STORMlikeChains',
-    'download_url'     : 'https://github.com/kmdouglass/STORMlikeChains',
+    'url'              : 'https://douglass@git.epfl.ch/repo/simmla.git',
+    'download_url'     : 'https://douglass@git.epfl.ch/repo/simmla.git',
     'author_email'     : 'kyle.douglass@epfl.ch',
     'version'          : '0.1.0a1',
     'install_requires' : ['numpy',
